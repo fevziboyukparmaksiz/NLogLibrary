20ac8d733567815bb2050d0fd351ac37f904c185
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-12 21:47:41 +0300

    Add project files.

---
+++ NlogLibrary/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NlogLibrary.Models;
+using System.Diagnostics;
+
+namespace NlogLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hello, this is the index!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NlogLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NlogLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
